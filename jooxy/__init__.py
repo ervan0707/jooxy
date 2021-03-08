4936d164b2741784ca156b822245f512c109e4bf
ervan <ervan@softwareseni.com>
2021-03-08 20:56:47 +0700

    refactor: remove unused files

--- jooxy/__init__.py
+++ jooxy/__init__.py
@@ -1,9 +1,9 @@
-__copyright__       = 'Copyright 2019 by Ervan R.F'
-__version__         = '1.0.2'
-__license__         = 'BSD-3-Clause'
-__author__          = 'Ervan R.F'
-__author_email__    = 'ervanroot@gmail.com'
-__url__             = 'http://github.com/ervan0707/jooxy'
+__copyright__ = "Copyright 2019 by Ervan R.F"
+__version__ = "1.0.2"
+__license__ = "BSD-3-Clause"
+__author__ = "Ervan R.F"
+__author_email__ = "ervanroot@gmail.com"
+__url__ = "http://github.com/ervan0707/jooxy"
 
 from .auth import Jooxy
 from .config import Config
